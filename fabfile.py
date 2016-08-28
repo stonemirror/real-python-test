ce148e158ecb3fe64535ac409479a7f9a84977ef
David Schlesinger <lefty@shugendo.org>
2016-08-27 20:40:31 -0700

    Added Fabric support for Heroku; implemented v1 REST API

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,12 @@
-from fabric.api import local
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
+
 
 def test():
-    local("nosetests -v")
+    with settings(warn_only=True):
+        result = local("nosetests -v", capture=True)
+    if result.failed and not confirm("Tests failed. Continue?"):
+        abort("Aborted at user request")
 
 
 def commit():
@@ -17,3 +22,27 @@ def prepare():
     test()
     commit()
     push()
+
+
+def pull():
+    local("git pull origin master")
+
+
+def heroku():
+    local("git push heroku master")
+
+
+def heroku_test():
+    local("heroku run nosetests -v")
+
+
+def deploy():
+    # pull()
+    test()
+    # commit()
+    heroku()
+    heroku_test()
+
+
+def rollback():
+    local("heroku rollback")
